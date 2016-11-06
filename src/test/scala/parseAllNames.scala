5f1b164fa2ce238537472fb00704322c3f52bc1f
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2016-11-06 19:28:46 +0100

    add basic node and names parsing tests

---
+++ src/test/scala/parseAllNames.scala
@@ -0,0 +1,19 @@
+package com.bio4j.data.ncbitaxonomy.test
+
+import org.scalatest.FunSuite
+import com.bio4j.data.ncbitaxonomy._, dmp._
+
+class ParseAllNames extends FunSuite {
+
+  def namesLines =
+    io.Source.fromFile("names.dmp").getLines
+
+  test("parse all names and access all data") {
+
+    dmp.names.fromLines(namesLines) foreach { n =>
+
+      val id   = n.nodeID
+      val name = n.name
+    }
+  }
+}
