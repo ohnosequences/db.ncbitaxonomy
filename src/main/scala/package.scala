9be72d0576d2c78b8598b0a5b5635ccec278f4c3
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2018-08-10 12:56:46 +0200

    add supported db versions

--- src/main/scala/package.scala
+++ src/main/scala/package.scala
@@ -1,8 +1,17 @@
 package ohnosequences.api
 
+import ohnosequences.db.ncbitaxonomy.Version
+
 package object ncbitaxonomy {
+
+  type DBVersion = Version
+
+  val dbVersions: Set[DBVersion] = {
+
+    import Version._
+    Set(_0_0_1, _0_1_0)
+  }
 
   private[ncbitaxonomy] type +[A, B] =
     Either[A, B]
-
 }
