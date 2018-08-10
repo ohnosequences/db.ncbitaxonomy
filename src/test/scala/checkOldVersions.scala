f0291204a6f590011283908514921ac5c93cfde0
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2018-08-10 13:34:02 +0200

    add tests for existence of old versions S3 objects

---
+++ src/test/scala/checkOldVersions.scala
@@ -0,0 +1,25 @@
+package ohnosequences.db.ncbitaxonomy.test
+
+import ohnosequences.db.ncbitaxonomy.{Version, names, nodes}
+import ohnosequences.awstools.s3, s3.ScalaS3Client
+
+class CheckOldVersions extends org.scalatest.FunSuite {
+
+  private val s3Client = ScalaS3Client(s3.defaultClient)
+
+  test("S3 objects exist for all versions") {
+
+    val versionObjects =
+      Version.all map { version =>
+        (version, names(version), nodes(version))
+      }
+
+    assert {
+      versionObjects forall {
+        case (version, namesObj, nodesObj) =>
+          println(s"S3 objects exist: ${version.name}")
+          s3Client.objectExists(namesObj) && s3Client.objectExists(nodesObj)
+      }
+    }
+  }
+}
